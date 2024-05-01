572e6f2968320610563bc388b212526e0584ca41
Satishbyteshake <satish.sahu@byteshake.co.uk>
2024-05-01 22:19:54 +0530

    component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomePageComponent, title: 'Home Page'},
+  {path: 'about-us', component: AboutPageComponent, title: 'About Us'},
+  {path: 'contact-us', component: ContactPageComponent, title: 'Contact Us'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { NavbarComponent } from './pages/partials/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutPageComponent,
+    ContactPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
